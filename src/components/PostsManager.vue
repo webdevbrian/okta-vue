9cc16e9d289a400b61159be4a77e9e45ebffb6a1
Brian Kinney <webdevbrian@gmail.com>
2019-10-01 10:29:51 -0400

    Cruft Clean up

--- src/components/PostsManager.vue
+++ src/components/PostsManager.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container-fluid mt-4">
     <h1 class="h1">Posts Manager</h1>
-    <p>Only authenticated users should see this page</p>
+    <p>Handle all post manager UI, view & access here (should only be seen by authenticated users)</p>
   </div>
 </template>
